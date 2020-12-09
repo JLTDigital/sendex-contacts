d498eade8bf167e1f47fe959cee97ec487ed3cbb
JLTDigital <mrjltaft@gmail.com>
2020-12-09 22:42:13 +0000

    commit 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/pages/Home'
+import About from './components/pages/About'
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import ContactState from './context/contact/ContactState';
 
 const App = () => {
   return (
-    <div className="App">
-      My app
-    </div>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 }
 
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,89 @@
+import React, { useReducer } from 'react';
+import {v4 as uuid} from 'uuid';
+import contactContext from './contactContext';
+import contactReducer from './contactReducer';
+import { 
+  ADD_CONTACT, 
+  DELETE_CONTACT, 
+  SET_CURRENT, 
+  CLEAR_CURRENT, 
+  UPDATE_CONTACT, 
+  FILTER_CONTACTS, 
+  CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'India michaels',
+        email: 'im@mail.com',
+        phone: '07771975847',
+        type: 'personal'
+      },
+      {
+        id: 2,
+        name: 'Rosie Jones',
+        email: 'rj@mail.com',
+        phone: '07771999847',
+        type: 'personal'
+      },
+      {
+        id: 3,
+        name: 'Barney Stinson',
+        email: 'bs@mail.com',
+        phone: '07771390229',
+        type: 'professional'
+      }
+    ],
+    current: null
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Add Contact
+  const addContact = contact => {
+    contact.id = uuid();
+    dispatch({ type: ADD_CONTACT, payload: contact })
+  }
+
+  // Delete Contact
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id })
+  }
+
+  // Set Current Contact
+  const setCurrent = contact => {
+    dispatch({ type: SET_CURRENT, payload: contact })
+  }
+
+  // Clear Current Contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT })
+  }
+  // Update Contact
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact })
+  }
+  // Filter Contacts
+
+  // Clear Filter
+
+  return (
+    <contactContext.Provider
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact
+      }}>
+      { props.children}
+    </contactContext.Provider>
+  )
+}
+
+export default ContactState
