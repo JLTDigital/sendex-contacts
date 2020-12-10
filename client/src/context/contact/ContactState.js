a4cc804a8006fa1e8736b751c671bad81df9396b
JLTDigital <mrjltaft@gmail.com>
2020-12-10 17:17:45 +0000

    update

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,7 +37,8 @@ const ContactState = props => {
         type: 'professional'
       }
     ],
-    current: null
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -67,19 +68,28 @@ const ContactState = props => {
     dispatch({ type: UPDATE_CONTACT, payload: contact })
   }
   // Filter Contacts
+  const filterContact = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text })
+  }
 
   // Clear Filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER })
+  }
 
   return (
     <contactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
-        updateContact
+        updateContact,
+        filterContact,
+        clearFilter
       }}>
       { props.children}
     </contactContext.Provider>
